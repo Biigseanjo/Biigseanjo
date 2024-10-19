10ca3d13b3c514991e0afa4c80ac2bf656289b93
Biigseanjo <147234441+Biigseanjo@users.noreply.github.com>
2024-10-19 10:24:39 +0530

    Update Live Graph.py

--- Live Graph.py
+++ Live Graph.py
@@ -18,15 +18,7 @@ def animate(i):
     charts=[]
     plt.cla()
     for file in fileNames:
-        file_path = os.path.join(dataDir, file)
-        df = pd.read_csv(file_path, index_col = 0)
-        plt.xticks(rotation=30)
-        chart,_=plt.plot(df,label=file)
-        charts.append(chart)
-    plt.legend(loc='upper left',handles=charts)
-    
-
-
+        
 ani = FuncAnimation(plt.gcf(), animate, interval=1000)
 #plt.gca().axes.get_xaxis().set_visible(False)
 plt.show()
